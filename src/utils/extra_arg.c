deab57cd1f8b2a377c4f48572fbcfb229cda16fd
Vratislav Podzimek <vpodzime@redhat.com>
2016-04-12 10:37:17 +0200

    Better name

--- src/utils/extra_arg.c
+++ src/utils/extra_arg.c
@@ -4,12 +4,12 @@
 #include "extra_arg.h"
 
 /**
- * bd_util_extra_arg_copy:
+ * bd_extra_arg_copy:
  *
  * Creates a new copy of @arg.
  */
-BDUtilExtraArg* bd_util_extra_arg_copy (BDUtilExtraArg *arg) {
-    BDUtilExtraArg *ret = g_new0 (BDUtilExtraArg, 1);
+BDExtraArg* bd_extra_arg_copy (BDExtraArg *arg) {
+    BDExtraArg *ret = g_new0 (BDExtraArg, 1);
     ret->opt = g_strdup (arg->opt);
     ret->val = g_strdup (arg->val);
 
@@ -17,36 +17,36 @@ BDUtilExtraArg* bd_util_extra_arg_copy (BDUtilExtraArg *arg) {
 }
 
 /**
- * bd_util_extra_arg_free:
+ * bd_extra_arg_free:
  *
  * Frees @arg.
  */
-void bd_util_extra_arg_free (BDUtilExtraArg *arg) {
+void bd_extra_arg_free (BDExtraArg *arg) {
     g_free (arg->opt);
     g_free (arg->val);
     g_free (arg);
 }
 
-GType bd_util_extra_arg_get_type (void) {
+GType bd_extra_arg_get_type (void) {
     static GType type = 0;
 
     if (G_UNLIKELY (!type))
-        type = g_boxed_type_register_static ("BDUtilExtraArg",
-                                             (GBoxedCopyFunc) bd_util_extra_arg_copy,
-                                             (GBoxedFreeFunc) bd_util_extra_arg_free);
+        type = g_boxed_type_register_static ("BDExtraArg",
+                                             (GBoxedCopyFunc) bd_extra_arg_copy,
+                                             (GBoxedFreeFunc) bd_extra_arg_free);
 
     return type;
 }
 
 /**
- * bd_util_extra_arg_new: (constructor)
+ * bd_extra_arg_new: (constructor)
  * @opt: extra option
  * @val: value for the extra option @opt
  *
  * Returns: (transfer full): a new extra argument
  */
-BDUtilExtraArg* bd_util_extra_arg_new (gchar *opt, gchar *val) {
-    BDUtilExtraArg *ret = g_new0 (BDUtilExtraArg, 1);
+BDExtraArg* bd_extra_arg_new (gchar *opt, gchar *val) {
+    BDExtraArg *ret = g_new0 (BDExtraArg, 1);
     ret->opt = g_strdup (opt);
     ret->val = g_strdup (val);
 
--- src/utils/extra_arg.h
+++ src/utils/extra_arg.h
@@ -4,16 +4,16 @@
 #ifndef BD_UTILS_EXTRA_ARG
 #define BD_UTILS_EXTRA_ARG
 
-#define BD_UTIL_TYPE_EXTRA_ARG (bd_util_extra_arg_get_type ())
-GType bd_util_extra_arg_get_type ();
+#define BD_UTIL_TYPE_EXTRA_ARG (bd_extra_arg_get_type ())
+GType bd_extra_arg_get_type ();
 
-typedef struct BDUtilExtraArg {
+typedef struct BDExtraArg {
     gchar *opt;
     gchar *val;
-} BDUtilExtraArg;
+} BDExtraArg;
 
-BDUtilExtraArg* bd_util_extra_arg_copy (BDUtilExtraArg *arg);
-void bd_util_extra_arg_free (BDUtilExtraArg *arg);
-BDUtilExtraArg* bd_util_extra_arg_new (gchar *opt, gchar *val);
+BDExtraArg* bd_extra_arg_copy (BDExtraArg *arg);
+void bd_extra_arg_free (BDExtraArg *arg);
+BDExtraArg* bd_extra_arg_new (gchar *opt, gchar *val);
 
 #endif  /* BD_UTILS_EXTRA_ARG */
